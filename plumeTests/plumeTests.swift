2437bc0184f3b6b65615318a0c0ddfdff48bd9b1
Guillaume Voisin <guillaume.voisin29@gmail.com>
2025-11-24 14:37:15 +0100

    Initial Commit

---
+++ plumeTests/plumeTests.swift
@@ -0,0 +1,16 @@
+//
+//  plumeTests.swift
+//  plumeTests
+//
+//  Created by Guillaume on 2025. 11. 24..
+//
+
+import Testing
+
+struct plumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
